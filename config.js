29fdab5a3690ce58cbf2bc876621b182fffd3f36
oliverwood <bois.olive@gmail.com>
2026-03-31 20:02:50 +0200

    add

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.SITE_CONFIG = {
   brandName: "Atelier Rangement",
   amazonUrl: "REPLACE_WITH_YOUR_AMAZON_SPECIAL_LINK_OR_PRODUCT_URL",
-  pinterestTagId: "REPLACE_WITH_PINTEREST_TAG_ID",
+  pinterestTagId: "2612629233865",
   pinterestVerification: "REPLACE_WITH_PINTEREST_DOMAIN_VERIFY",
   defaultBoard: "Organisation cuisine",
   supportEmail: "bonjour@example.com"
